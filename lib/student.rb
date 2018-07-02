209d97c08412de1c154c4fc1901f1f6e50a911b4
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-02 12:34:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
+    students_array.each do |student_hash|
       student = Student.new(student_hash)
     end
   end
